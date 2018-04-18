859ce50891bab171c87b343b8652bf1f6859fa61
Bernat Romagosa <bromagosa@e-citilab.eu>
2018-04-18 12:20:00 +0200

    pairing working now

--- microblocks-adapter.js
+++ microblocks-adapter.js
@@ -12,236 +12,206 @@ const Adapter = require('../adapter');
 const Device = require('../device');
 const Property = require('../property');
 const SerialPort = require('serialport');
+const Protocol = require('./uprotocol.js');
+
+// Adapter
 
 class MicroBlocksProperty extends Property {
-  constructor(device, name, propertyDescription) {
-    super(device, name, propertyDescription);
-    this.unit = propertyDescription.unit;
-    this.description = propertyDescription.description;
-    this.setCachedValue(propertyDescription.value);
-    this.device.notifyPropertyChanged(this);
-  }
-
-  /**
-   * @method setValue
-   * @returns a promise which resolves to the updated value.
-   *
-   * @note it is possible that the updated value doesn't match
-   * the value passed in.
-   */
-  setValue(value) {
-    return new Promise((resolve, reject) => {
-      super.setValue(value).then((updatedValue) => {
-        resolve(updatedValue);
+    constructor(device, name, propertyDescription) {
+        super(device, name, propertyDescription);
+        this.unit = propertyDescription.unit;
+        this.description = propertyDescription.description;
+        this.setCachedValue(propertyDescription.value);
         this.device.notifyPropertyChanged(this);
-      }).catch((err) => {
-        reject(err);
-      });
-    });
-  }
+    }
+
+    /**
+     * @method setValue
+     * @returns a promise which resolves to the updated value.
+     *
+     * @note it is possible that the updated value doesn't match
+     * the value passed in.
+     */
+    setValue(value) {
+        return new Promise((resolve, reject) => {
+            super.setValue(value).then((updatedValue) => {
+                resolve(updatedValue);
+                this.device.notifyPropertyChanged(this);
+            }).catch((err) => {
+                reject(err);
+            });
+        });
+    }
 }
 
 class MicroBlocksDevice extends Device {
-  constructor(adapter, id, deviceDescription) {
-    super(adapter, id);
-    this.name = deviceDescription.name;
-    this.type = deviceDescription.type;
-    this.description = deviceDescription.description;
-    for (var propertyName in deviceDescription.properties) {
-      var description = deviceDescription.properties[propertyName];
-      var property = new MicroBlocksProperty(this, propertyName, description);
-      this.properties.set(propertyName, property);
+    constructor(adapter, id, deviceDescription) {
+        super(adapter, id);
+        this.name = deviceDescription.name;
+        this.type = deviceDescription.type;
+        this.description = deviceDescription.description;
+        for (var propertyName in deviceDescription.properties) {
+            var description = deviceDescription.properties[propertyName];
+            var property = new MicroBlocksProperty(this, propertyName, description);
+            this.properties.set(propertyName, property);
+        }
     }
-  }
 
-  notifyPropertyChanged(property) {
-    super.notifyPropertyChanged(property);
-    this.adapter.sendProperty(this.id, property);
-  }
+    notifyPropertyChanged(property) {
+        super.notifyPropertyChanged(property);
+        // this.adapter.sendProperty(this.id, property);
+    }
 }
 
 class MicroBlocksAdapter extends Adapter {
-  constructor(addonManager, packageName) {
-    super(addonManager, 'MicroBlocks', packageName);
+    constructor(addonManager, packageName) {
+        super(addonManager, 'MicroBlocks', packageName);
 
-    this.devices = new Map();
-    this.port = new SerialPort('/dev/ttyACM0', { baudRate: 115200 });
+        // boards are indexed by port name
+        this.boards = new Map();
 
-    this.receiveBuf = new Buffer(0);
-    this.onPortData = this.onPortData.bind(this);
-    this.port.on('data', this.onPortData);
+        //this.receiveBuf = new Buffer(0);
+        //this.onPortData = this.onPortData.bind(this);
+        //this.port.on('data', this.onPortData);
 
-    addonManager.addAdapter(this);
+        addonManager.addAdapter(this);
 
-	// xxx temporary
-    this.addLED = this.addLED.bind(this);
-	this.addLED();
-  }
-
-  addLED() {
-      let deviceDescription = {
-      name: 'User LED',
-      type: 'onOffLight',
-      properties: {
-        on: {
-          name: 'on',
-          type: 'boolean',
-          value: false,
-        },
-      },
-    };
-
-    if (!this.devices.has(deviceDescription.name)) {
-      var device = new MicroBlocksDevice(this, deviceDescription.name, deviceDescription);
-      this.handleDeviceAdded(device);
-      this.devices.set(deviceDescription.name, device);
-    }
-  }
-
-  addLED2() {
-      let deviceDescription = {
-		  name: 'Dimmable LED',
-		  type: 'dimmableLight',
-		  properties: {
-			on: {
-			  name: 'on',
-			  type: 'boolean',
-			  value: false,
-			},
-			level: {
-			  name: 'level',
-			  type: 'number',
-			  value: 0,
-			},
-		  },
-    };
-
-    if (!this.devices.has(deviceDescription.name)) {
-      var device = new MicroBlocksDevice(this, deviceDescription.name, deviceDescription);
-      this.handleDeviceAdded(device);
-      this.devices.set(deviceDescription.name, device);
-    }
-  }
-
-  onPortData(data) {
-    this.receiveBuf = Buffer.concat([this.receiveBuf, data]);
-
-    let deviceDescription = {
-      name: 'User LED',
-      type: 'dimmableLight',
-      properties: {
-        on: {
-          name: 'on',
-          type: 'boolean',
-          value: false,
-        },
-		level: {
-		  name: 'level',
-		  type: 'number',
-		  value: 0,
-		},
-      },
-    };
-
-    if (!this.devices.has(deviceDescription.name)) {
-      var device = new MicroBlocksDevice(this, deviceDescription.name, deviceDescription);
-      this.handleDeviceAdded(device);
-      this.devices.set(deviceDescription.name, device);
+        // xxx temporary
+        //this.addLED = this.addLED.bind(this);
+        //this.addLED();
     }
 
+    startPairing(_timeoutSeconds) {
+        console.log('MicroBlocks adapter pairing started');
+        var myself = this;
+        SerialPort.list().then(function (ports) {
+            ports.forEach(function (port) {
+                if (port.vendorId) {
+                    // test this port to see if there's a µBlocks device in it
+                    var serialPort = new SerialPort(port.comName, { baudRate: 115200 }),
+                        protocol = new Protocol(serialPort);
+
+                    serialPort.receiveBroadcast = function (message) {
+                        var description = JSON.parse(message);
+                        myself.addBoard(port.comName, description.name, description.devices);
+                    };
+
+                    serialPort.on('data', function (data) {
+                        protocol.processRawData(Array.from(new Uint8Array(data)));
+                    });
+                    console.log('probing ' + port.comName);
+                    var message = protocol.packMessage('broadcast', 0, protocol.packString('moz-pair'));
+                    console.log(message);
+                    serialPort.write(message);
+                }
+            });
+        })
+    }
+
+    /**
+     * Cancel the pairing/discovery process.
+     */
+    cancelPairing() {
+        console.log('ExampleAdapter:', this.name, 'id', this.id,
+                'pairing cancelled');
+    }
+
+    addBoard(portName, boardName, deviceDescriptors) {
+        var myself = this;
+        console.log('found board: ' + boardName);
+        // TODO if board already exists, update it
+        if (!this.boards.has(portName)) {
+            // TODO what to do with board name?
+            var devices = new Map();
+            this.boards.set(portName, devices);
+            deviceDescriptors.forEach(function (deviceDescriptor) {
+                // TODO if device already exists, update it
+                var device = new MicroBlocksDevice(myself, deviceDescriptor.name, deviceDescriptor);
+                myself.handleDeviceAdded(device);
+                devices.set(deviceDescriptor.name, device);
+            });
+        }
+    }
     /*
-    if (broadcast of the json blob) {
-      let blob = {
-        name: 'example-plug-2',
-        type: 'onOffSwitch',
-        description: 'MicroBlocks Plugin Device',
-        properties: {
-          on: {
-            name: 'on',
-            type: 'boolean',
-            value: false,
-          },
-          level: {
-            name: 'level',
-            type: 'number',
-            value: 0,
-          },
-        },
-      };
-      var device = new MicroBlocksDevice(this, blob.name, blob);
-      this.handleDeviceAdded(device);
+
+    onPortData(data) {
+        this.receiveBuf = Buffer.concat([this.receiveBuf, data]);
+
+        let deviceDescription = {
+            name: 'User LED',
+            type: 'dimmableLight',
+            properties: {
+                on: {
+                    name: 'on',
+                    type: 'boolean',
+                    value: false,
+                },
+                level: {
+                    name: 'level',
+                    type: 'number',
+                    value: 0,
+                },
+            },
+        };
+
+        if (!this.devices.has(deviceDescription.name)) {
+            var device = new MicroBlocksDevice(this, deviceDescription.name, deviceDescription);
+            this.handleDeviceAdded(device);
+            this.devices.set(deviceDescription.name, device);
+        }
     }
     */
-  }
 
-  /**
-   * For cleanup between tests.
-   */
-  clearState() {
-    this.actions = {};
-    for (let deviceId in this.devices) {
-      this.removeDevice(deviceId);
+    /**
+     * For cleanup between tests.
+     */
+    clearState() {
+        this.actions = {};
+        for (let deviceId in this.devices) {
+            this.removeDevice(deviceId);
+        }
     }
-  }
 
-  /**
-   * Add a MicroBlocksDevice to the MicroBlocksAdapter
-   *
-   * @param {String} deviceId ID of the device to add.
-   * @return {Promise} which resolves to the device added.
-   */
-  addDevice(deviceId, deviceDescription) {
-    return new Promise((resolve, reject) => {
-      if (deviceId in this.devices) {
-        reject('Device: ' + deviceId + ' already exists.');
-      } else {
-        var device = new MicroBlocksDevice(this, deviceId, deviceDescription);
-        this.handleDeviceAdded(device);
-        resolve(device);
-      }
-    });
-  }
-
-  /**
-   * Remove a MicroBlocksDevice from the MicroBlocksAdapter.
-   *
-   * @param {String} deviceId ID of the device to remove.
-   * @return {Promise} which resolves to the device removed.
-   */
-  removeDevice(deviceId) {
-    return new Promise((resolve, reject) => {
-      var device = this.devices[deviceId];
-      if (device) {
-        this.handleDeviceRemoved(device);
-        resolve(device);
-      } else {
-        reject('Device: ' + deviceId + ' not found.');
-      }
-    });
-  }
-
-  sendProperty(deviceId, property) {
-    if (property.name == 'on') {
-	  let varID = 0;
-      this.sendLongMessage(0x08, varID, [3, (property.value ? 1 : 0)]);
-    } else if (property.name == 'level') {
-	  let varID = 1;
-      let n = Math.floor(property.value);
-      this.sendLongMessage(0x08, varID,
-      	[1, (n & 0xff), ((n >> 8) & 0xff), ((n >> 16) & 0xff), ((n >> 24) & 0xff)]);
+    /**
+     * Add a MicroBlocksDevice to the MicroBlocksAdapter
+     *
+     * @param {String} deviceId ID of the device to add.
+     * @return {Promise} which resolves to the device added.
+     */
+    addDevice(deviceId, deviceDescription) {
+        return new Promise((resolve, reject) => {
+            if (deviceId in this.devices) {
+                reject('Device: ' + deviceId + ' already exists.');
+            } else {
+                var device = new MicroBlocksDevice(this, deviceId, deviceDescription);
+                this.handleDeviceAdded(device);
+                resolve(device);
+            }
+        });
     }
-  }
 
-  sendLongMessage(opcode, varID, data) {
-    data.push(0xfe);
-    let buf = Buffer.from(
-	  [0xfb, opcode, varID, data.length & 0xff, (data.length >> 16) & 0xff].concat(data));
-    this.port.write(buf);
-  }
+    /**
+     * Remove a MicroBlocksDevice from the MicroBlocksAdapter.
+     *
+     * @param {String} deviceId ID of the device to remove.
+     * @return {Promise} which resolves to the device removed.
+     */
+    removeDevice(deviceId) {
+        return new Promise((resolve, reject) => {
+            var device = this.devices[deviceId];
+            if (device) {
+                this.handleDeviceRemoved(device);
+                resolve(device);
+            } else {
+                reject('Device: ' + deviceId + ' not found.');
+            }
+        });
+    }
+
 }
 
 function loadMicroBlocksAdapter(addonManager, manifest, _errorCallback) {
-  var adapter = new MicroBlocksAdapter(addonManager, manifest.name);
+    var adapter = new MicroBlocksAdapter(addonManager, manifest.name);
 }
 
 module.exports = loadMicroBlocksAdapter;
---
+++ uprotocol.js
@@ -0,0 +1,390 @@
+/*
+
+    protocol.js
+
+    µBlocks protocol support for Snap!
+
+
+    written by John Maloney, Jens Mönig, and Bernat Romagosa
+    http://microblocks.fun
+
+    Copyright (C) 2018 by John Maloney, Jens Mönig, Bernat Romagosa
+
+    This file is part of Snap!.
+
+    Snap! is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of
+    the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+function detect(list, predicate) {
+    // answer the first element of list for which predicate evaluates
+    // true, otherwise answer null
+    var i, size = list.length;
+    for (i = 0; i < size; i += 1) {
+        if (predicate.call(null, list[i])) {
+            return list[i];
+        }
+    }
+    return null;
+}
+
+var Protocol;
+
+// Protocol //////////////////////////////////////////////////
+
+// µBlocks message protocol
+// I interpret and dispatch messages received via the µBlocks postal service
+
+function Protocol (owner) {
+    this.init(owner);
+};
+
+Protocol.prototype.init = function (owner) {
+    this.messageBuffer = [];
+    this.owner = owner;
+};
+
+Protocol.prototype.processRawData = function (data) {
+    this.messageBuffer = this.messageBuffer.concat(data);
+    this.parseMessage();
+};
+
+Protocol.prototype.clearBuffer = function () {
+    this.messageBuffer = [];
+};
+
+Protocol.prototype.parseMessage = function () {
+    var check = this.messageBuffer[0],
+        isLong = check == 0xFB,
+        opCode = this.messageBuffer[1],
+        descriptor, dataSize;
+
+    if (check !== 0xFA && check !== 0xFB) {
+        // We probably connected to the board while it was sending a message
+        // and missed its header.
+        this.clearBuffer();
+        return;
+    }
+
+    if (!opCode) {
+        // We haven't yet gotten our opCode, let's wait for it.
+        return;
+    }
+
+    descriptor = this.descriptorFor(opCode);
+
+    if (isLong && this.messageBuffer.length >= 5) {
+        dataSize = this.messageBuffer[3] | this.messageBuffer[4] << 8;
+        if (this.messageBuffer.length >= dataSize + 5) {
+            // The message is complete, let's parse it.
+            this.processMessage(descriptor, dataSize);
+            this.messageBuffer = this.messageBuffer.slice(5 + dataSize);
+            this.parseMessage();
+        }
+    } else if (!isLong && this.messageBuffer.length >= 3) {
+        // this message carries no data and is complete
+        this.processMessage(descriptor);
+        this.messageBuffer = this.messageBuffer.slice(3);
+        this.parseMessage();
+    }
+};
+
+Protocol.prototype.processMessage = function (descriptor, dataSize) {
+    var data,
+        taskId = this.messageBuffer[2];
+
+    if (dataSize) {
+        data = this.messageBuffer.slice(5, 5 + dataSize);
+    }
+
+    if (descriptor.selector === 'jsonMessage') {
+        value =
+            this.processJSONMessage(JSON.parse(String.fromCharCode.apply(null, data)));
+    } else {
+        if (dataSize) {
+            this.dispatcher[descriptor.selector].call(this, data, taskId);
+        } else {
+            this.dispatcher[descriptor.selector].call(this, taskId);
+        }
+    }
+};
+
+Protocol.prototype.processJSONMessage = function (json) {
+    this.dispatcher[json.selector].apply(
+        this.owner,
+        json.arguments
+    );
+};
+
+Protocol.prototype.processReturnValue = function (rawData) {
+    var type = rawData[0],
+        value;
+
+    if (type === 1) {
+        // integer
+        value = (rawData[4] << 24) | (rawData[3] << 16) | (rawData[2] << 8) | (rawData[1]);
+    } else if (type === 2) {
+        // string
+        value = this.processString(rawData.slice(1));
+    } else if (type === 3) {
+        // boolean
+        value = rawData.slice(1) == 1;
+    }
+
+    return isNil(value) ? 'unknown type' : value;
+};
+
+Protocol.prototype.processString = function (rawData) {
+    return String.fromCharCode.apply(null, rawData);
+};
+
+Protocol.prototype.processErrorValue = function (rawData) {
+    // rawData[0] contains the error code
+    return this.descriptorFor('taskError').dataDescriptor[rawData[0]] || 'Unspecified Error';
+};
+
+Protocol.prototype.packString = function (string) {
+    return string.split('').map(function (char) { return char.charCodeAt(0); });
+};
+
+Protocol.prototype.packMessage = function (selector, taskId, data) {
+    var descriptor = this.descriptorFor(selector),
+        message = [data ? 0xFB : 0xFA, descriptor.opCode, taskId];
+
+    if (data) {
+        if (descriptor.isLong) {
+            data = data.concat(0xFE);
+        }
+        // add the data size in little endian
+        message = message.concat(data.length & 255).concat((data.length >> 8) & 255);
+        // add the data
+        message = message.concat(data);
+    }
+
+    return message;
+};
+
+Protocol.prototype.descriptorFor = function (selectorOrOpCode) {
+    return detect(
+        this.descriptors,
+        function (descriptor) {
+            if (typeof selectorOrOpCode === 'string') {
+                return descriptor.selector === selectorOrOpCode;
+            } else {
+                return descriptor.opCode === selectorOrOpCode;
+            }
+        }
+    );
+};
+
+// Message descriptors
+
+Protocol.prototype.descriptors = [
+    // IDE → Board
+    {
+        opCode: 0x01,
+        selector: 'storeChunk',
+        isLong: true
+    },
+    {
+        opCode: 0x02,
+        selector: 'deleteChunk'
+    },
+    {
+        opCode: 0x03,
+        selector: 'startChunk'
+    },
+    {
+        opCode: 0x04,
+        selector: 'stopChunk'
+    },
+    {
+        opCode: 0x05,
+        selector: 'startAll'
+    },
+    {
+        opCode: 0x06,
+        selector: 'stopAll'
+    },
+    {
+        opCode: 0x07,
+        selector: 'getVar'
+    },
+    {
+        opCode: 0x08,
+        selector: 'setVar',
+        isLong: true
+    },
+    {
+        opCode: 0x0A,
+        selector: 'deleteVar'
+    },
+    {
+        opCode: 0x0B,
+        selector: 'deleteComment'
+    },
+    {
+        opCode: 0x0C,
+        selector: 'getVmVersion'
+    },
+    {
+        opCode: 0x0D,
+        selector: 'getAllChunks'
+    },
+    {
+        opCode: 0x0E,
+        selector: 'deleteAll'
+    },
+    {
+        opCode: 0x0F,
+        selector: 'systemReset'
+    },
+
+    // Board → IDE
+    {
+        opCode: 0x10,
+        selector: 'taskStarted'
+    },
+    {
+        opCode: 0x11,
+        selector: 'taskDone'
+    },
+    {
+        opCode: 0x12,
+        selector: 'taskReturned',
+        isLong: true
+    },
+    {
+        opCode: 0x13,
+        selector: 'taskError',
+        dataDescriptor: {
+            0: 'No error',
+            1: 'Unspecified error',
+            2: 'Bad chunk index',
+            10: 'Insufficient memory to allocate object',
+            11: 'Needs an Array or ByteArray',
+            12: 'Needs a boolean',
+            13: 'Needs an integer',
+            14: 'Needs a string',
+            15: 'Those objects cannot be compared for equality',
+            16: 'Array size must be a non-negative integer',
+            17: 'Array index must be an integer',
+            18: 'Array index out of range',
+            19: 'A ByteArray can only store integer values between 0 and 255',
+            20: 'Hexadecimal input must between between -1FFFFFFF and 1FFFFFFF',
+            21: 'I2C device ID must be between 0 and 127',
+            22: 'I2C register must be between 0 and 255',
+            23: 'I2C value must be between 0 and 255',
+            24: 'Attempt to access argument or local variable outside of a function',
+        },
+        isLong: true
+    },
+    {
+        opCode: 0x14,
+        selector: 'outputValue',
+        isLong: true
+    },
+    {
+        opCode: 0x15,
+        selector: 'variableValue',
+        isLong: true
+    },
+    {
+        opCode: 0x16,
+        selector: 'vmVersion',
+        isLong: true
+    },
+    // Bidirectional
+    {
+        opCode: 0x1A,
+        selector: 'ping'
+    },
+    {
+        opCode: 0x1B,
+        selector: 'broadcast',
+        isLong: true
+    },
+    {
+        opCode: 0x1C,
+        selector: 'chunkAttribute',
+        dataDescriptor: {
+            0: 'chunkPosition',
+            1: 'snapSource',
+            2: 'gpSource'
+        },
+        isLong: true
+    },
+    {
+        opCode: 0x1D,
+        selector: 'varName',
+        isLong: true
+    },
+    {
+        opCode: 0x1E,
+        selector: 'comment',
+        isLong: true
+    },
+    {
+        opCode: 0x1F,
+        selector: 'commentPosition',
+        isLong: true
+    },
+
+    // Bridge → IDE
+    {
+        opCode: 0xFF,
+        selector: 'jsonMessage'
+    }
+];
+
+Protocol.prototype.dispatcher = {
+    // JSON messages
+    getSerialPortListResponse: function (portList) {
+    },
+    serialConnectResponse: function (success, portPath) {
+    },
+    serialDisconnectResponse: function (success, portPath) {
+    },
+    boardUnplugged: function (portPath) {
+    },
+    boardReconnected: function (portPath) {
+    },
+
+    // µBlocks messages
+    taskStarted: function (taskId) {
+    },
+    taskDone: function (taskId) {
+    },
+    taskReturned: function (data, taskId) {
+    },
+    taskError: function (data, taskId) {
+    },
+    variableValue: function (data, varIndex) {
+        console.log('var ' + this.varIndex + ' is ' + this.processReturnValue(data));
+    },
+    outputValue: function (data, taskId) {
+        console.log('task ' + taskId + ' says ' + this.processReturnValue(data));
+    },
+    broadcast: function (data, taskId) {
+        this.owner.receiveBroadcast(this.processString(data));
+    },
+    vmVersion: function (data) {
+        var versionString = this.processString(data),
+            vmVersion = versionString.substring(1).replace(/ .*/,''),
+            boardType = versionString.replace(/.* /, '');
+        console.log('board is a ' + boardType + ' and runs µBlocks ' + vmVersion);
+    }
+};
+
+module.exports = Protocol;
