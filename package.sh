25a3f2a71742ac7cd2921f13a7267c4902c11536
James Hobin <hobinjk@gmail.com>
2018-04-25 14:09:11 -0400

    Add correct addon packaging

--- package.sh
+++ package.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+rm -rf node_modules
+npm install --production
 rm -f SHA256SUMS
-sha256sum *.json *.js LICENSE > SHA256SUMS
-npm pack
+shasum -a 256 package.json *.js LICENSE > SHA256SUMS
+find node_modules -type f -exec shasum -a 256 {} \; >> SHA256SUMS
+TARFILE=$(npm pack)
+tar xzf ${TARFILE}
+cp -r node_modules ./package
+tar czf ${TARFILE} package
+rm -rf package
+echo "Created ${TARFILE}"
